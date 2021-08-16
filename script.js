2a99462daf9165912f4236b8a809a28313eac9fb
Mad1flin2 <75647159+Mad1flin2@users.noreply.github.com>
2021-08-16 10:43:46 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+function setup() {
 createCanvas(300, 300);
-createWebcam(300, 300);
+createCapture(300, 300);
+}
+function draw() {
 rect(300, 300, 55, 55);
 rect(0, 300, 55, 55);
 rect(300, 0, 55, 55);
 rect(0, 0, 55, 55);
+}
